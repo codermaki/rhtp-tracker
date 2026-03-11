40a17e653ef4845cdc747706b290b41e1ff16cc6
codermaki <mlivadas@gmail.com>
2026-03-10 21:28:04 -0400

    Delete pages directory

--- pages/api/data.js
+++
@@ -1,9 +0,0 @@
-import { getStateData, getFederalData } from "../../lib/storage";
-
-export default async function handler(req, res) {
-  const [stateData, federalData] = await Promise.all([
-    getStateData(),
-    getFederalData(),
-  ]);
-  return res.status(200).json({ stateData, federalData });
-}
